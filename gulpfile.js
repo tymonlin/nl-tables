efac45f2176120ad9b26f83e0cb4faa5cedf8fd2
林春晖 <titanic_33@sina.com>
2015-12-30 15:47:39 +0800

    插件描述修改

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,32 @@ gulp.task('minify', function() {
         .pipe(ngmin({dynamic: false}))
         .pipe(stripDebug())
         .pipe(uglify({outSourceMap: false, mangle: true}))
-        .pipe(concat('all.min.js'))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('help',function () {
+
+    console.log('	gulp build			文件打包');
+
+    console.log('	gulp watch			文件监控打包');
+
+    console.log('	gulp help			gulp参数说明');
+
+    console.log('	gulp server			测试server');
+
+    console.log('	gulp -p				生产环境（默认生产环境）');
+
+    console.log('	gulp -d				开发环境');
+
+    console.log('	gulp -m <module>		部分模块打包（默认全部打包）');
+
+});
+
+/* 默认 */
+
+gulp.task('default',function () {
+
+    gulp.start('help');
+
 });
